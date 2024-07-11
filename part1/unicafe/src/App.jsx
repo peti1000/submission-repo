30f029b633f20a24b7b8338e70e75f866e714c9e
peti1000 <petije@gmail.com>
2024-07-11 17:50:04 +0200

    unicafe step 6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,58 +1,80 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-const Button = ( {handleClick, text} ) => <button onClick={handleClick}>{text}</button>
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
 
-const StatisticLine = ( {value, text} ) => {
-  if (text == 'positive') {
-    return <p>{text} {value} %</p>
+const StatisticLine = ({ value, text }) => {
+  if (text == "positive") {
+    return (
+      <tr>
+        <td>{text}</td>
+        <td>{value} %</td>
+      </tr>
+    );
   }
-  return <p>{text} {value}</p>
-}
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
 
-const Statistics = ( props ) => {
+const Statistics = (props) => {
   if (props.all == 0) {
-    return <p>No feedback given</p>
+    return <p>No feedback given</p>;
   }
-  return(
+  return (
     <>
-      <StatisticLine text='good' value={(props.good)}/>
-      <StatisticLine text='neutral' value={(props.neutral)}/>
-      <StatisticLine text='bad' value={(props.bad)}/>
-      <StatisticLine text='all' value={(props.all)}/>
-      <StatisticLine text='average' value={(props.average)}/>
-      <StatisticLine text='positive' value={(props.positive)}/>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={props.good} />
+          <StatisticLine text="neutral" value={props.neutral} />
+          <StatisticLine text="bad" value={props.bad} />
+          <StatisticLine text="all" value={props.all} />
+          <StatisticLine text="average" value={props.average} />
+          <StatisticLine text="positive" value={props.positive} />
+        </tbody>
+      </table>
     </>
-  )
-}
+  );
+};
 
 const App = () => {
-  
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  const handleGoodClicks = () => setGood(good + 1)
-  const handleNeutralClicks = () => setNeutral(neutral + 1)
-  const handleBadClicks = () => setBad(bad + 1)
+  const handleGoodClicks = () => setGood(good + 1);
+  const handleNeutralClicks = () => setNeutral(neutral + 1);
+  const handleBadClicks = () => setBad(bad + 1);
 
-  const all = good + neutral + bad
-  const average = (good - bad) / all
-  const positive = good / all * 100
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const positive = (good / all) * 100;
 
   return (
     <div>
       <h1>give feedback</h1>
       <div>
-        <Button handleClick={handleGoodClicks} text='good'/>
-        <Button handleClick={handleNeutralClicks} text='neutral'/>
-        <Button handleClick={handleBadClicks} text='bad'/>
+        <Button handleClick={handleGoodClicks} text="good" />
+        <Button handleClick={handleNeutralClicks} text="neutral" />
+        <Button handleClick={handleBadClicks} text="bad" />
       </div>
       <h1>statistics</h1>
       <div>
-        <Statistics good={good} neutral={neutral} bad={bad} average={average} all={all} positive={positive}/>
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          average={average}
+          all={all}
+          positive={positive}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
