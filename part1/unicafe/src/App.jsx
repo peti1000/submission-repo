044fe4e30b3e7e308a5ef7c2ed20936987d02cc6
peti1000 <petije@gmail.com>
2024-07-11 17:14:38 +0200

    unicafe step 4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,10 +4,24 @@ const Button = ( {handleClick, text} ) => <button onClick={handleClick}>{text}</
 
 const Display = ( {value, text} ) => <p>{text} {value}</p>
 
-const Statistics = ( {statistic, text} ) => <p>{text} {statistic}</p>
+const Statistics = ( props ) => {
+  if (props.all == 0) {
+    return <p>No feedback given</p>
+  }
+  return(
+    <>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all {props.all}</p>
+      <p>average {props.average}</p>
+      <p>positive {props.positive}%</p>
+    </>
+  )
+}
 
 const App = () => {
-  // save clicks of each button to its own state
+  
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -30,12 +44,7 @@ const App = () => {
       </div>
       <h1>statistics</h1>
       <div>
-        <Display value={good} text='good'/>
-        <Display value={neutral} text='neutral'/>
-        <Display value={bad} text='bad'/>
-        <Statistics statistic={all} text='all'/>
-        <Statistics statistic={average} text='average'/>
-        <Statistics statistic={positive} text='positive'/>
+        <Statistics good={good} neutral={neutral} bad={bad} average={average} all={all} positive={positive}/>
       </div>
     </div>
   )
